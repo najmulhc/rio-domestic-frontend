ecb1f580b515976bde0e52f34495e7a22d54a1dc
Najmul Huda Chowdhury Chayan <87802203+najmulhc@users.noreply.github.com>
2023-03-01 00:34:31 +0600

    Product card added.

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,5 +1,28 @@
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+import './ProductCard.css'
+const ProductCard = ({product}) => {
+  
+  const { name, brand, ram, screen, processor } = product;
+  const handleAddToCart = e => {
+    e.preventDefault();
+    console.log(product)
+  }
+  return (
+    <div className='product-container'>
+      <div className='top-container'>
+        <h2 className='item-name'>{name}</h2>
+        <span className='brand-badge'>{brand}</span>
+      </div>
+      <div className='spec-container'>
+        <h3>Specifications</h3>
+        <p className='spec'>RAM: {ram}GB</p>
+        <p className='spec'>Screen: {screen.size} inch {screen.type}</p>
+        <p className='spec'>Processor: {processor}</p>
+      </div>
+      <button className='cart-button' type='submit' onClick={handleAddToCart}>
+        Add To Cart
+      </button>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-const Home = () => {
-  const string = 'Khalid Farhan Home';
-  return <div>
-    
-    </div>;
+import ProductCard from '../components/productCard/ProductCard';
+
+const Home = () => { 
+  const product = {
+    name: 'Iphone 14',
+    brand: 'apple',
+    ram: 6,
+    screen:  {
+      size: 6.7, 
+      type: "Retina"
+    },
+    processor: 'apple a15 bionic',
+  };
+  return (
+    <div className='main-container'>
+      <ProductCard product={product} /> 
+    </div>
+  );
 };
 
 export default Home;
